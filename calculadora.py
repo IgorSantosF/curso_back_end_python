20265712d75585790cefb5790c0109255885065d
IgorSantosF <igor.dossantosferreira98@gmail.com>
2025-08-20 21:05:33 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,15 @@
+A = int(input("Digite o primeiro valor: "))  #Solicitação de valores para operação
+B = int(input("Digite o segundo valor: "))
+
+soma = A + B  #Operações utilizadas
+subtracao = A - B 
+multiplicacao = A * B
+divisao = A / B 
+
+print("Se eu somar {} com {}, meu resultado é {}".format(A,B,soma))   #Resultados
+print("Se eu subtrair {} de {}, meu resultado será {}".format(A,B,subtracao))
+print("Se eu multiplicar {} por {}, meu resultado é {}".format(A,B,multiplicacao))
+print(f"Se eu dividir {A} por {B}, meu resultado será {divisao}")
+
+
+
