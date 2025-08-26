0c59a4060e9776e3067dfe7fb262977f5e61f9bf
Igor <igor.dossantosferreira98@gmail.com>
2025-08-25 21:14:21 -0300

    Add files via upload
    Prática de laços de repetição básicos.

---
+++ projeto_lacos_de_repeticao.py
@@ -0,0 +1,10 @@
+numero = int(input( "Digite um número e teste sua sorte: "))
+
+if numero <= 3 :
+    print( " Não se preocupe com o futuro, ele já está rindo de você também" )
+
+elif 3 < numero < 7 :
+    print( " Sorte do dia: hoje ninguém vai te pedir dinheiro emprestado! " )
+
+else:
+    print( " O importante não é vencer, é fazer parecer que tentou! " )
