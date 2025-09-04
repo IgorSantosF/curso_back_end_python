dcaf02627a3bed20552233f2bba4668b732c60f7
Igor <igor.dossantosferreira98@gmail.com>
2025-09-03 21:04:41 -0300

    Add files via upload

---
+++ ache_o_queijo.py
@@ -0,0 +1,30 @@
+
+print("ACHE O QUEIJO")
+
+local_queijo = "banheiro"
+
+local = input("Digite um local: sala, banheiro ou quarto: ")
+
+while local != local_queijo:
+    print ("Queijo não encontrado")
+    local = input("Digite uma nova opção: ")
+
+print(f"Queijo encontrado no {local}")
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
